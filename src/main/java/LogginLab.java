6b182d8a7ddb69a7ce8e918d63055953818e5bf1
igordon215 <igordon215@gmail.com>
2024-07-04 19:11:17 -0400

    method made

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -34,8 +34,11 @@ public class LogginLab {
         else
             return false;
     }
-
+    public boolean thresholdReached(Integer limit) {
+        return this.threshold > limit;
+    }
+}
     // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
-}
+
